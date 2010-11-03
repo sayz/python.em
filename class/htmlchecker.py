ac798bc8fc61c7b1dc8e44c440fa448558c82105
emineker <emineker@bil.omu.edu.tr>
2010-11-03 19:14:06 +0200

    html checker

---
+++ class/htmlchecker.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re, sys
+
+def sade(okunan):
+        liste = re.findall(r'<[/]*\w+[^>]*>', okunan)
+        liste = [i for i in liste if i[-2]!='/']
+
+        tag, kapa = list(), list()
+
+        for i in liste:
+                if ' ' in i:
+                        tag.append(i[1:i.find(' ')])
+                elif ' ' not in i and '/' not in i:
+                        tag.append(i[1:i.find('>')])
+                else:
+                        kapa.append(i[2:i.find('>')])
+        return (tag, kapa)
+
+
+if __name__ == "__main__":
+        #dosya = sys.argv[1]
+        dosya = 'deneme.html'
+        with open(dosya) as dosya:
+                okunan = dosya.read().decode("utf8")
+
+        (tag, kapa) = sade(okunan)
+        for i in tag:
+                if i not in kapa:
+                        print 'açılan bir', i, 'tagı kapanmamış'
+                elif okunan.find('<'+i) > okunan.find('</%s>' %i):
+                        print i, "tagı önce kapanmış sonra açılmış"
+                        kapa.remove(i)
+                else:
+                        kapa.remove(i)
+
+        for i in kapa:
+                print 'daha önce açılmayan bir', i, 'tagı kapanmış'
+
+
